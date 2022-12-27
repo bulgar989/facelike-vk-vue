23753c118f18607a9010947e98e7877901334690
Rinat Sibgatullin <38031619+bulgar989@users.noreply.github.com>
2022-12-27 14:52:43 +0300

    api server

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 const Api = axios.create({
-    baseURL: 'http://localhost:8010/proxy/',
+    baseURL: 'https://facelike.bmlabs.ru/api/',
+    // baseURL: 'http://localhost:8010/proxy/',
 })
 
 export default Api
